17b3a047a2ed921a9e2d0c013f16279cf8a35614
Miklós Tusz <mdtusz@gmail.com>
2020-04-03 11:04:40 -0400

    Add triggered rendering using a `Condvar`
    To minimize wasted render cycles, a `Condvar` has been added that is
    only triggered when either the application audio engine state has
    changed, or when a user input has been received. This allows for evented
    rendering to display changes nearly as quickly as they occur.
    
    To prevent lockups in the render loop and when consuming user inputs, a
    timeout has been added to the wait condition such that the UI will run
    at ~60fps. While this works for now, fast streams of characters are
    processed frame-by-frame, rather than being batched, so the input loop
    should be modified to read continuously until the iterator blocks before
    triggering the render event.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use std::io;
 use std::io::Write;
-use std::sync::{mpsc, Arc, Mutex};
+use std::sync::{mpsc, Arc, Condvar, Mutex};
 use std::thread;
+use std::time::Duration;
 
 use dsp::sample::ToFrameSliceMut;
 use dsp::{Graph, Node, NodeIndex};
@@ -21,11 +22,6 @@ mod engine;
 mod ui;
 
 use engine::{DspNode, Oscillator, Output, Wave, CHANNELS, FRAMES, SAMPLE_HZ};
-
-#[derive(Debug, PartialEq)]
-enum Message {
-    Done,
-}
 
 struct App {
     graph: Graph<[Output; CHANNELS], DspNode>,
@@ -55,10 +51,18 @@ enum Mode {
 
 impl App {
     fn new() -> Self {
-        let graph = Graph::new();
+        let mut graph = Graph::new();
         let ui = UiState::new();
 
+        let master = graph.add_node(DspNode::Gain(1.0));
+        graph.set_master(Some(master));
+
         Self { graph, ui }
+    }
+
+    fn master(&self) -> &DspNode {
+        let master_index = self.graph.master_index().expect("No master gain!");
+        self.graph.node(master_index).unwrap()
     }
 }
 
@@ -69,14 +73,15 @@ fn main() -> Result<(), pa::Error> {
     // let mut graph = Graph::new();
     let mut app = App::new();
 
-    let master = app.graph.add_node(DspNode::Gain(1.0));
-    app.graph.set_master(Some(master));
-
-    let mut osc1 = Oscillator::new(Wave::Sine, 440.0, 0.0);
-    let (_, osc1n) = app.graph.add_input(DspNode::Oscillator(osc1), master);
+    // let mut osc1 = Oscillator::new(Wave::Sine, 440.0, 0.0);
+    // let (_, osc1n) = app.graph.add_input(DspNode::Oscillator(osc1), master);
 
     // Prepare graph for concurrency.
-    let graph = Arc::new(Mutex::new(app.graph));
+    let pair = Arc::new((Mutex::new(app), Condvar::new()));
+
+    let audio_pair = Arc::clone(&pair);
+    let ui_pair = Arc::clone(&pair);
+    let input_pair = Arc::clone(&pair);
 
     let callback = move |pa::OutputStreamCallbackArgs { buffer, .. }| {
         let buffer: &mut [[Output; CHANNELS]] = buffer.to_frame_slice_mut().unwrap();
@@ -84,8 +89,11 @@ fn main() -> Result<(), pa::Error> {
         // Insert silence to start.
         dsp::slice::equilibrium(buffer);
 
+        let (app, trigger) = &*audio_pair;
+        let mut app = app.lock().unwrap();
+
         // Compute audio from graph.
-        graph.lock().unwrap().audio_requested(buffer, SAMPLE_HZ);
+        app.graph.audio_requested(buffer, SAMPLE_HZ);
 
         pa::Continue
     };
@@ -102,16 +110,19 @@ fn main() -> Result<(), pa::Error> {
     let backend = TermionBackend::new(stdout);
     let mut terminal = Terminal::new(backend).unwrap();
 
-    terminal.clear();
+    // Clear and hide the cursor to start!
+    terminal.clear().unwrap();
     terminal.hide_cursor().unwrap();
 
     // Input thread.
     let (tx, rx) = mpsc::channel();
     thread::spawn(move || loop {
+        let (_, trigger) = &*input_pair;
         let stdin = io::stdin();
         match stdin.keys().next().unwrap() {
             Ok(key) => {
                 tx.send(key).unwrap();
+                trigger.notify_all();
             }
             _ => {}
         }
@@ -119,6 +130,14 @@ fn main() -> Result<(), pa::Error> {
 
     // UI thread
     loop {
+        let (app, trigger) = &*ui_pair;
+        let mut app = app.lock().unwrap();
+        let result = trigger
+            .wait_timeout(app, Duration::from_millis(16))
+            .unwrap();
+
+        app = result.0;
+
         match rx.try_recv() {
             Ok(key) => match key {
                 Key::Char('q') => {
@@ -144,6 +163,7 @@ fn main() -> Result<(), pa::Error> {
         };
 
         let ui = app.ui.clone();
+        drop(app);
 
         terminal.draw(|mut f| {
             let chunks = Layout::default()
